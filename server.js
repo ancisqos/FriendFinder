bb3922d1218433ad0f911bc8decce3b3b27d9101
Francis <ancis.arcede@gmail.com>
2017-07-12 20:25:29 -0400

    set up express

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-// Dependencies
+// DEPENDENCIES
 // =======================================================
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-// Sets up Express App
+// SETS UP EXPRESS APP
 // =======================================================
 const app = express();
-
 const PORT = 9001;
 
+
+// BODYPARSER
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({
-	type: "application/vnd.api+json"
-}));
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-// Set up data
+// ROUTES
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
+// LISTENER
+
+app.listen(PORT, function(err) {
+	if (err) {
+		return console.error(err)
+	}
+
+	console.log("App listening on PORT " + PORT);
+});
